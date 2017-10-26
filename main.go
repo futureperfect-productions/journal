bb8ec591465ed936bd7b9870cb583f399cadac8c
xanderseren <xander@theactualschool.com>
2017-10-26 16:49:53 -0400

    adds template rendering and initial form submission

--- main.go
+++ main.go
@@ -1,9 +1,73 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"net/http"
+	"path/filepath"
+
+	"gopkg.in/mgo.v2/bson"
+)
+
+type Entry struct {
+	ID    bson.ObjectId `bson:"_id,omitempty"`
+	Title string        `bson:"title" json:"title"`
+}
+
+func AddEntry(w http.ResponseWriter, r *http.Request) {
+	session := Connect()
+	defer session.Close()
+
+	collection := session.DB("fp-journal").C("entries")
+
+	title := r.FormValue("Title")
+	e := Entry{}
+	e.Title = title
+
+	// Populate the user data
+	json.NewDecoder(r.Body).Decode(&e)
+
+	// Write the entry to mongo
+	collection.Insert(e)
+
+	ej, _ := json.Marshal(e)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(201)
+	fmt.Fprintf(w, "%s", ej)
+}
+
+func GetEntries(w http.ResponseWriter, r *http.Request) {
+	session := Connect()
+	defer session.Close()
+
+	collection := session.DB("fp-journal").C("entries")
+	var results []Entry
+
+	if err := collection.Find(nil).All(&results); err != nil {
+		w.WriteHeader(404)
+		return
+	}
+
+	e, _ := json.Marshal(results)
+
+	// Write content-type, statuscode, payload
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	fmt.Fprintf(w, "%s", e)
+}
 
 func main() {
-	session := Connect()
+	http.HandleFunc("/", serveTemplate)
+	http.HandleFunc("/save", AddEntry)
+	http.HandleFunc("/entries", GetEntries)
+	http.ListenAndServe(":8080", nil)
+}
 
-	fmt.Println("vim-go")
+func serveTemplate(w http.ResponseWriter, r *http.Request) {
+	lp := filepath.Join("templates", "layout.html")
+
+	tmpl, _ := template.ParseFiles(lp)
+	tmpl.ExecuteTemplate(w, "layout", nil)
 }
--- utils.go
+++ utils.go
@@ -3,15 +3,34 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	mgo "gopkg.in/mgo.v2"
 )
 
 func Connect() (session *mgo.Session) {
-	connectURL := "url"
-	session, err := mgo.Dial(connectURL)
+
+	var (
+		MongoDBHosts = os.Getenv("MONGO_URI")
+		AuthDatabase = "fp-journal"
+		AuthUserName = os.Getenv("MONGO_USER")
+		AuthPassword = os.Getenv("MONGO_PASS")
+	)
+
+	mongoDBDialInfo := &mgo.DialInfo{
+		Addrs:    []string{MongoDBHosts},
+		Timeout:  600 * time.Second,
+		Database: AuthDatabase,
+		Username: AuthUserName,
+		Password: AuthPassword,
+	}
+
+	// Connect to DB with credentials, temporarily using
+	// mLab for this initial implementation
+	session, err := mgo.DialWithInfo(mongoDBDialInfo)
 	if err != nil {
-		fmt.Printf("Cannot connect to MongoDB, %v\n", err) os.Exit(1)
+		fmt.Printf("Cannot connect to MongoDB, %v\n", err)
+		os.Exit(1)
 	}
 
 	session.SetSafe(&mgo.Safe{})
